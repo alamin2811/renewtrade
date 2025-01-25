86eee5314ea4677909acac25b3cef1f424076bf1
alamin2811 <alaminbinmonsur@gmail.com>
2025-01-25 09:59:18 +0600

    modfied

---
+++ src/Component/App/Home/Index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../../Core/Header/Index'
+import BannerOne from './BannerOne'
+import BuyCard from './BuyCard'
+
+const HomeCompoennt = () => {
+  return (
+    <div className='home-page'>
+        <Header/>
+        <BannerOne/>
+    </div>
+  )
+}
+
+export default HomeCompoennt
