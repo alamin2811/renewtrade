de84333f57673131a8704db4a117a9408136c81d
unknown <alaminbinmonsur@gmail.com>
2025-01-25 13:40:01 +0600

    modified

--- src/Component/App/Home/Index.jsx
+++ src/Component/App/Home/Index.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import Header from '../../Core/Header/Index'
 import BannerOne from './BannerOne'
 import BuyCard from './BuyCard'
+import WhatIs from './WhatIs'
+import HowItWorks from './HowItWorks'
 
 const HomeCompoennt = () => {
   return (
     <div className='home-page'>
         <Header/>
         <BannerOne/>
+        <WhatIs/>
+        <HowItWorks/>
     </div>
   )
 }
